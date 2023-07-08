df9c78675383672a2335ce7404597688b002510a
Kevin O'Connell <kevinoconnell42@gmail.com>
2023-07-08 15:04:05 -0700

    add addresses

--- frontend/lib/consts.ts
+++ frontend/lib/consts.ts
@@ -2,3 +2,7 @@ export const APP_NAME = "Purple";
 export const COOKIE_NAME = "purple-session";
 export const purpleNFTTokenAddress =
   "0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60";
+export const auctionHouseContractAddress =
+  "0x43790fe6bd46b210eb27F01306C1D3546AEB8C1b";
+export const governanceContractAddress =
+  "0xFB4A96541E1C70FC85Ee512420eB0B05C542df57";
