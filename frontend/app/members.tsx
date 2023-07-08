b8ed70bba1a5fcc5b4600084577bbbfa6146b356
Kevin O'Connell <kevinoconnell42@gmail.com>
2023-07-08 14:40:22 -0700

    axios and call

--- frontend/app/members.tsx
+++ frontend/app/members.tsx
@@ -4,7 +4,7 @@ import { User } from "@standard-crypto/farcaster-js";
 export async function getMembers(): Promise<(User | undefined)[]> {
   const client = generateClient("aisjadj9fj"); // TODO: get key from server
   const members: string[] = []; // TODO: get members from alchemy
-  return Promise.all(members.map(client.lookupUserByVerification));
+  return await Promise.all(members.map(client.lookupUserByVerification));
 }
 
 export default async function Members() {
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,4 +1,40 @@
-export const tap = async <T>(value: T, cb: (value: T) => Promise<unknown>): Promise<T> => {
+import axios from "axios";
+
+import { User } from "@standard-crypto/farcaster-js";
+export const tap = async <T>(
+  value: T,
+  cb: (value: T) => Promise<unknown>
+): Promise<T> => {
   await cb(value);
   return value;
+};
+
+export const getMembers = async (
+  client: any
+): Promise<(User | undefined)[]> => {
+  const members = await getPurpleMembers();
+  return await Promise.all(members.map(client.lookupUserByVerification));
+};
+
+export const getPurpleMembers = async () => {
+  try {
+    const response = await axios({
+      method: "get",
+      url: `https://eth-mainnet.g.alchemy.com/nft/v3/${
+        process.env.NEXT_PUBLIC_ALCHEMY_API as string
+      }/getOwnersForContract`,
+      params: {
+        contractAddress: "0xa45662638e9f3bbb7a6fecb4b17853b7ba0f3a60",
+        withTokenBalances: false,
+      },
+      headers: {
+        accept: "application/json",
+      },
+    });
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 };
