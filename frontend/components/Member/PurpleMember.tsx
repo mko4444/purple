a09f2d646c3b9a9ba2646288ba52f54ad2b35fe3
Matthew <mrk443@stern.nyu.edu>
2023-08-06 09:37:29 -0700

    use next/link and route to etherscan if no farcaster account

--- frontend/components/Member/PurpleMember.tsx
+++ frontend/components/Member/PurpleMember.tsx
@@ -2,31 +2,36 @@
 import React from "react";
 import Image from "next/image";
 import Badge from "../Badge";
+import Link from "next/link";
 
 type PurpleMemberProps = {
   member: any;
 };
 
 const PurpleMember: React.FC<PurpleMemberProps> = ({ member }) => {
+  const username = member.farcaster.username;
+  const hasFCUser = !username.startsWith("0x");
+
   return (
     member && (
-      <button
-        className="row-fs-c"
-        onClick={() => {
-          window.open(`https://www.warpcast.com/${member.username}`, "_blank");
-        }}
-        style={{ maxWidth: "200px" }}
+      <Link
+        className="w-100"
+        href={hasFCUser ? `https://warpcast.com/${username}` : `https://etherscan.io/address/${member.address}`}
       >
-        <Image
-          alt={member.farcaster.username}
-          src={member.farcaster.pfp?.url}
-          height={36}
-          width={36}
-          style={{ borderRadius: 100 }}
-        />
-        <div>{member.farcaster.username}</div>
-        <Badge>⨉ {member.daoTokenCount}</Badge>
-      </button>
+        <button className="row-fs-c w-100">
+          <Image
+            alt={member.farcaster.username}
+            src={member.farcaster.pfp?.url}
+            height={36}
+            width={36}
+            style={{ borderRadius: 100 }}
+          />
+          <div className="flex line-1" style={{ textAlign: "left" }}>
+            {member.farcaster.username}
+          </div>
+          <Badge>⨉ {member.daoTokenCount}</Badge>
+        </button>
+      </Link>
     )
   );
 };
